5b8c9e0583b8994e91ab5d5eae797cc775c37f3d
LeandroPereira25 <a10961@agrcanelas.com>
2026-01-07 10:58:14 +0000

    adiçao do login no index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- Barra de Navegação -->
     <div class="barra-navegacao">
         <a class="ativo" href="#">Início</a>
-        <a href="#">Link 1</a>
+        <a href="formlogin.php">Login</a>
         <a href="#">Link 2</a>
         <a href="#">Link 3</a>
         <a href="#">Link 4</a>
