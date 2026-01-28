73be0f73d1eb41ea70736b7db02aa71cdd44d30d
LeandroPereira25 <a10961@agrcanelas.com>
2026-01-28 19:30:07 +0000

    Remove main app and user management files
    Deleted core PHP files, CSS stylesheets, SQL schema, and authentication scripts from the root directory. These files handled user login, registration, profile management, and the main dashboard, indicating a major refactor or migration of the application structure.

--- menu.php
+++
@@ -1,7 +0,0 @@
-<div class="barra-navegacao">
-        <a class="ativo" href="index.php">Início</a>
-        <a href="formlogin.php">Login</a>
-        <a href="animais.php">Animais</a>
-        <a href="#">Link 3</a>
-        <a href="#">Link 4</a>
-    </div>
